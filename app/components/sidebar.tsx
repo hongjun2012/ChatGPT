47133cb3d0f4114766b521ac8acdc4227c244c42
hongjun2012 <hongjun.tu@gmail.com>
2023-06-17 11:09:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          上峰云能人工智能
         </div>
         <div className={styles["sidebar-sub-title"]}>
           上峰云能ChatGPT
